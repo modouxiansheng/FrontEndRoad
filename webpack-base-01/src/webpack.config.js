d9f3f2cefb8bea7fbb9c6df02200b677ee7fb7a2
“hu_pf” <hu_pf@suixingpay.com>
2019-12-04 18:16:18 +0800

    《后端也要懂一点前端系列》使用webpack搭建项目

--- webpack-base-01/src/webpack.config.js
+++ webpack-base-01/src/webpack.config.js
@@ -1,3 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin') // 导入在内存中自动生成index页面的插件
+const path = require('path')
+
+// 创建一个插件的实例对象
+const htmlplugin = new HtmlWebpackPlugin({
+    template: path.join(__dirname,'./src/index.html'), // 源文件
+    filename: 'index.html'
+})
+
+// 向外暴露一个打包的配置对象
 module.exports = {
-    mode: 'development'
+    mode: 'development',
+    plugins:[
+        htmlplugin
+    ]
 }
