bbb4326b4bff6564a14bf40f3a833c312db33fd6
“hu_pf” <hu_pf@suixingpay.com>
2019-12-05 15:06:45 +0800

    webpack01

--- webpack-base-01/src/index.js
+++ webpack-base-01/src/index.js
@@ -1,1 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
 console.log("ok")
+
+var myH1 = React.createElement('h1', null, '这是一个大大的H1')
+
+var myDiv = React.createElement('div', { title: 'this is a div', id: 'mydiv' }, '这是一个div', myH1)
+
+ReactDOM.render(myDiv, document.getElementById('app'))
